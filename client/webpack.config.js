6f5643cefd01e89daa9801ade23422e0c5a01949
Morgan Kelly <mkelly5380@gmail.com>
2022-11-30 08:45:35 -0600

    fixed service worker in injectManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -26,7 +23,7 @@ module.exports = () => {
       // added InjectManifest 
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'service-worker.js',
+        swDest: 'src-sw.js',
       }),
       // added manifest.json 
       new WebpackPwaManifest({
